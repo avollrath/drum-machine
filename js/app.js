0c6cdc96ef6c8950c757b694769d88f85063866c
André Vollrath <46895425+avollrath@users.noreply.github.com>
2019-09-18 23:41:52 +0300

    finished basic functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,40 @@
 const allBtns = document.querySelectorAll(".buttons__btn");
 const allIndicators = document.querySelectorAll(".buttons__indicator");
 
+
 let step = 1;
+
+kick  = new Switcher('../assets/sounds/kick.wav', 8);
+snare  = new Switcher('../assets/sounds/snare.wav', 8);
+tom  = new Switcher('../assets/sounds/tom.wav', 8);
+hihat  = new Switcher('../assets/sounds/hihat.wav', 8);
+
+
+function Channel(audio_uri) {
+	this.audio_uri = audio_uri;
+	this.resource = new Audio(audio_uri);
+}
+
+Channel.prototype.play = function() {
+	this.resource.play();
+}
+
+function Switcher(audio_uri, num) {
+	this.channels = [];
+	this.num = num;
+	this.index = 0;
+
+	for (var i = 0; i < num; i++) {
+		this.channels.push(new Channel(audio_uri));
+	}
+}
+
+Switcher.prototype.play = function() {
+	this.channels[this.index++].play();
+	this.index = this.index < this.num ? this.index : 0;
+}
+
+
 
 // add light toggle to buttons
 
@@ -11,29 +44,49 @@ allBtns.forEach(btn =>
   })
 );
 
+setInterval(function() {
+  allBtns.forEach(btn => {
+    if (
+      btn.classList.contains("lit") &&
+      btn.dataset.row == 1 &&
+      btn.dataset.btn == step
+    ){
+      kick.play();
+      btn.style.filter = "brightness(1.3)";
+      setTimeout(function() {btn.style.filter = "none";}, 60);}
+    if (
+      btn.classList.contains("lit") &&
+      btn.dataset.row == 2 &&
+      btn.dataset.btn == step
+    ) {
+      snare.play();
+      btn.style.filter = "brightness(1.3)";
+      setTimeout(function() {btn.style.filter = "none";}, 60);}
+    if (
+      btn.classList.contains("lit") &&
+      btn.dataset.row == 3 &&
+      btn.dataset.btn == step
+    ){
+      tom.play();
+      btn.style.filter = "brightness(1.3)";
+      setTimeout(function() {btn.style.filter = "none";}, 60);}
+    if (
+      btn.classList.contains("lit") &&
+      btn.dataset.row == 4 &&
+      btn.dataset.btn == step
+    ) {
+      hihat.play();
+      btn.style.filter = "brightness(1.3)";
+      setTimeout(function() {btn.style.filter = "none";}, 60);}
+  });
 
+  allIndicators.forEach(indicator => {
+    indicator.classList.remove("indi-lit");
 
+    if (indicator.dataset.indicator == step) {
+      indicator.classList.toggle("indi-lit");
+    }
+  });
 
-setInterval(function(){ 
-    
-    
-
-    allIndicators.forEach(indicator => {
-
-        indicator.classList.remove("indi-lit");
-
-        if (indicator.dataset.indicator == step) {
-          indicator.classList.toggle("indi-lit");
-        }
-      });
-    
-    
-        step < 8 ? step++ : step = 1;
-
-
-
+  step < 8 ? step++ : (step = 1);
 }, 300);
-
-
-
-
